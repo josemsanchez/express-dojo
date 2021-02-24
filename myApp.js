d7f0df1707afb9cade815ea348549b63cfad96f3
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-02-24 14:32:27 +0000

    A first working Express server

--- myApp.js
+++ myApp.js
@@ -4,9 +4,10 @@ var app = express();
 /** 1)Meet the Node console */
 console.log("Hello World");
 
-
-
-
+/** 2)A first working Express server */
+app.get('/', (request, response) => {
+    response.send("Hello Express")
+})
 
 
 
