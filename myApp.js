cee9e0974025da89b0492391bad5ccb16f618ee2
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-02-25 16:14:28 +0000

    3)Serve an HTML file

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 /** 1)Meet the Node console */
-console.log("Hello World");
+//console.log("Hello World");
 
 /** 2)A first working Express server */
 /*app.get('/', (request, response) => {
