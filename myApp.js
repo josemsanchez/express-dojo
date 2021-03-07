707daa3b28eb145572710d46055c538f80e8aec2
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-03-07 13:25:52 +0000

    Serve static assets

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 /** 1)Meet the Node console */
-//console.log("Hello World");
+console.log("Hello World");
 
 /** 2)A first working Express server */
 /*app.get('/', (request, response) => {
@@ -14,7 +14,8 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + "/views/index.html")
 })
 
-
+/** 4)Serve static assets */
+app.use(express.static(__dirname + '/public'))
 
 
 
