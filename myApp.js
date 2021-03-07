8bea5c3323777fdfd5d5f2c510a684070aeeb2e1
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-03-07 14:10:15 +0000

    Use the .env to configure the app

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 var express = require('express');
+require('dotenv').config();
 var app = express();
 
 /** 1)Meet the Node console */
@@ -19,12 +20,18 @@ app.use("/public", express.static(__dirname + '/public'))
 
 /** 5)Serve JSON on a Specific Route */
 let message = {message: "Hello json"};
-app.get('/json', (request, response) => {
+/*app.get('/json', (request, response) => {
 	response.json(message)
+})*/
+
+/** 6)Use the .env to configure the app */
+app.get('/json', (request, response) => {
+	if (process.env.MESSAGE_STYLE === 'uppercase'){
+        response.json({message: "HELLO JSON"})
+    }else {
+        response.json(message)
+    }
 })
-
-
-
 
 
 
